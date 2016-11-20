faaff0819076d74370114ace531600987a74a6b8
dhan12 <emaildhan@gmail.com>
2016-11-20 10:50:19 -0500

    refactor variable names

--- README.md
+++ README.md
@@ -7,9 +7,12 @@ Save history_archive.bash to your machine.
 Add the following to your `~/.bashrc`
 
     source ~/Projects/HistoryArchive/history_archive.bash
-    PROMPT_COMMAND="${PROMPT_COMMAND:-:} ; PostCommand;"
 
 Change the path to history_archive.bash to match your file path.
 
 # How this works
-history_archive.bash sets up a trap to save the current command "$(BASH_COMMAND)". Then it saves the timestamp, the current directory and this command to a file in ~/history_archive/*.txt files.
+history_archive.bash sets up a `trap` to capture the current command "$(BASH_COMMAND)". This command, along with the current timestamp and directory, are written to a txt file in ~/history_archive/.
+
+
+# TODO
+- fix multiple statement in one line bug
--- history_archive.bash
+++ history_archive.bash
@@ -4,65 +4,76 @@
 
 # Each file will contain commands that are grouped together by date/time.
 # Change this variable to put more or less commands in a given file.
-num_of_days_per_file=7
+NUM_OF_DAYS_PER_FILE__HA=7
 
 
 # Name of directory to put files.
-# Files will be put into $(HOME)/archive_name
-archive_name=history_archive
+# Files will be put into $(HOME)/ARCHIVE_NAME__HA
+ARCHIVE_NAME__HA=history_archive
 
 
 # Function that will run prior to each command
 function PreCommand() {
   current_command=$BASH_COMMAND
-  if [ -z "$AT_PROMPT" ]; then
+  # echo "in PreCommand $current_command"
+  if [ -z "$READY_TO_ARCHIVE__HA" ]; then
     return
   fi
-  unset AT_PROMPT
+  unset READY_TO_ARCHIVE__HA
 
+  # echo "in PreCommand $current_command (saving)"
   #
   # Execute the following before prompt is drawn.
   #
 
-  # Get date/time information
-  time_stamp=$(date +%Y%m%d%H%M%S)
-  day=$(date +%d)
-  setDayForHistoryFile $day
-  archive_file="$archive_dir/$(date +%Y%m)$dayForHistoryFile.txt"
+  # Set the archive_file.
+  # It must be called every time. It changes over time.
+  SetCurrentFile
 
   # Save data to the file
-  command_path=$(pwd)
-  line="$time_stamp $command_path $current_command" 
-  echo "$line" >> $archive_file
+  line="$time_stamp $(pwd) $current_command"
+  echo "$line" >> $ARCHIVE_FILE
 }
 trap "PreCommand" DEBUG
 
 # Function that will run after each command.
 # It just sets variables so that grouped commands like
-#   $ cmd1 && cmd2 && cmd3 
+#   $ cmd1 && cmd2 && cmd3
 # will only be processed once.
-FIRST_PROMPT=1
 function PostCommand() {
-  AT_PROMPT=1
+  # echo "in PostCommand"
+  READY_TO_ARCHIVE__HA=1
+}
 
-  if [ -n "$FIRST_PROMPT" ]; then
-    unset FIRST_PROMPT
-    return
+
+# Function to get current history file
+function SetCurrentFile() {
+
+  time_stamp=$(date +%Y%m%d%H%M%S)
+  day=$(date +%d)
+
+  # Get the day for the history file.
+  dayForHistoryFile=$(( ( ( (day-1) /NUM_OF_DAYS_PER_FILE__HA) * \
+    NUM_OF_DAYS_PER_FILE__HA) + 1 ))
+  if [ $dayForHistoryFile -lt 10 ]; then
+    dayForHistoryFile="0$dayForHistoryFile"
   fi
+
+  ARCHIVE_FILE="$archive_dir/$(date +%Y%m)$dayForHistoryFile.txt"
+  touch $ARCHIVE_FILE
 }
 
-
-# Get the day for the history file.
-function setDayForHistoryFile() {
-    dayForHistoryFile=$(( ( ( (day-1) /num_of_days_per_file) * \
-        num_of_days_per_file) + 1 )) 
-    if [ $dayForHistoryFile -lt 10 ]; then
-        dayForHistoryFile="0$dayForHistoryFile"
-    fi
-}
 
 # Get the directory to save data to
 current_user=$(whoami)
 home_dir=$(eval echo ~${current_user})
-archive_dir="$home_dir/$archive_name"
+archive_dir="$home_dir/$ARCHIVE_NAME__HA"
 mkdir -p $archive_dir
+
+
+# Some convenience functions
+alias ha='tail -100 $ARCHIVE_FILE'
+
+
+# Activate the history_archive.
+PROMPT_COMMAND="${PROMPT_COMMAND:-:} ; PostCommand;"
