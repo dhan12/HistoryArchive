f28da65d0640db2d7e17916aa43c0959a5be6996
dhan12 <emaildhan@gmail.com>
2017-01-05 07:36:26 -0500

    Add all parser option

--- ha_commands.py
+++ ha_commands.py
@@ -7,6 +7,12 @@ import argparse
 def getLatest(fileDir):
     return max(glob.iglob(fileDir+'/*.txt'), key=os.path.getctime)
 
+def getFiles(fileDir, getAll):
+    if getAll:
+        return sorted(list(glob.iglob(fileDir+'/*.txt')))
+    else:
+        return [getLatest(fileDir)]
+
 def parseArgs():
     parser = argparse.ArgumentParser(description='History Archive commands', prog="ha")
     parser.add_argument('-d', '--dir', required=True,
@@ -15,26 +21,34 @@ def parseArgs():
     parser.add_argument('-c', '--comments', action='store_true',
             help='Show recent comments',
             dest='getComments')
-
+    parser.add_argument('-a', '--all', action='store_true',
+            help='Show output from all files (WARNING, this may be slow)',
+            dest='getAll')
     return parser.parse_args()
 
 def main():
     args = parseArgs()
 
     if args.getComments:
-        latestFile = getLatest(args.historyArchiveDir)
-        proc = subprocess.Popen(['tail','-1000',latestFile], stdout=subprocess.PIPE)
-        out, err = proc.communicate()
-        for line in out.split('\n'):
-            try:
-                index = line.index('#')
-                # print YYYYMMDD HH:MM #comment
-                print line[:8],line[8:10]+':'+line[10:12],line[index:]
-            except:
-                pass
+        for f in getFiles(args.historyArchiveDir, args.getAll):
+            if args.getAll:
+                proc = subprocess.Popen(['cat',f], stdout=subprocess.PIPE)
+            else:
+                proc = subprocess.Popen(['tail','-1000',f], stdout=subprocess.PIPE)
+            out, err = proc.communicate()
+            for line in out.split('\n'):
+                try:
+                    index = line.index('#')
+                    # print YYYYMMDD HH:MM #comment
+                    print line[:8],line[8:10]+':'+line[10:12],line[index:]
+                except:
+                    pass
     else:
-        latestFile = getLatest(args.historyArchiveDir)
-        subprocess.call(['tail','-1000',latestFile])
+        for f in getFiles(args.historyArchiveDir, args.getAll):
+            if args.getAll:
+                subprocess.call(['cat',f])
+            else:
+                subprocess.call(['tail','-1000',f])
 
 if __name__ == '__main__':
     main()
